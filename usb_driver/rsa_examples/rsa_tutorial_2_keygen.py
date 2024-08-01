382ce6390025bc3947ee0c3e30bfd9c98933937f
BobtheElf <74628352+BobtheElf@users.noreply.github.com>
2024-08-01 13:50:10 -0600

    Adding all of the keys for all devices

--- usb_driver/rsa_examples/rsa_tutorial_2_keygen.py
+++ usb_driver/rsa_examples/rsa_tutorial_2_keygen.py
@@ -4,8 +4,8 @@ import rsa
 public_key, private_key = rsa.newkeys(1024)
 
 #Save the key pair into files
-with open("hmi_key.pub", "wb") as f:
+with open("scada_pub_key.pem", "wb") as f:
     f.write(public_key.save_pkcs1("PEM"))
-with open("hmi_key.prv", "wb") as f:
+with open("scada_prv_key.pem", "wb") as f:
     f.write(private_key.save_pkcs1("PEM"))
     
