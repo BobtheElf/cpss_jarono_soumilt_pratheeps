c8c1795c62c940d727ac35d7af0614ada233d419
BobtheElf <jaronogr@gmail.com>
2024-08-01 01:20:30 -0600

    Getting inputs from the pico over usb, and I can print them out

--- usb_device/bad_setup/cpss_bad_script_2.py
+++ usb_device/bad_setup/cpss_bad_script_2.py
@@ -1,23 +1,3 @@
-
-'''
-
-This is a pico macro keyboard based on CircuitPython adafruit_hid library
-The PCB is currently in version one and thus the key layout is all crazy
-like this:
-
-    ****This will be fixed in Final version***
-
-         key[0]    Key[3]   Key[6]
-
-         key[1]    Key[4]   Key[7]
-
-         key[2]    Key[5]   Key[8]
-
-         key[9]    Key[10]   Key[11]
-
-'''
-#Import all the relevant Libraries
-
 import time
 import board
 import digitalio
@@ -29,6 +9,7 @@ from adafruit_hid.consumer_control import ConsumerControl
 from adafruit_hid.consumer_control_code import ConsumerControlCode
 
 import random
+import sys
 
 # Set up Consumer Control - Control Codes can be found here: https://docs.circuitpython.org/projects/hid/en/latest/_modules/adafruit_hid/consumer_control_code.html#ConsumerControlCode
 cc = ConsumerControl(usb_hid.devices)
@@ -47,10 +28,9 @@ for x in range(0,len(buttons)):
     key[x].pull = digitalio.Pull.DOWN
 
 
-time.sleep(5.0)
-write_text.write("Hello, World! Press the button to write \"Hello, World!\"")
+
 while True:
     if key[0].value:
-        write_text.write("Hello, World!\n")
+        print("Hello, World!\n")
         time.sleep(0.3)
     time.sleep(0.1)
--- usb_driver/usb_driver.py
+++ usb_driver/usb_driver.py
@@ -1,12 +1,55 @@
-print ("Hello, World!")
-import serial
+#!usr/bin/python
 
-#turn this into a while loop
-ser = serial.Serial('/dev/ttyACM0',9600)
-s = [0,1]
+import serial
+import subprocess, sys
+
+def wait_for_usb():
+    # Code will break when the new usb device is detected
+    new_device = False
+
+    #Loop through and only break when there is a new usb device
+    while not new_device:
+        
+        #Get the usb devices
+        result = subprocess.check_output("lsusb", text = True)
+        devices = result.split('\n')
+        num_devices = len(devices)
+        #If the last 'device' is just a '', get rid of it
+        if devices[num_devices - 1] == "":
+            devices = devices[0 : num_devices - 1]
+            num_devices = num_devices - 1
+        # print(num_devices, end = "\n")
+        # print(devices)
+        
+        #Determine if there is a new device plugged in
+        whitelist = ['1d6b:0003',
+                     '046d:c534',
+                     '04d9:0006',
+                     '05e3:0610',
+                     '2109:3431',
+                     '1d6b:0002']
+        for device in devices:
+            if device[23 : 32] not in whitelist:
+                new_device = True
+                break
+    print("New Device Detected")
+#Start the main block
+wait_for_usb()
+ser = serial.Serial('/dev/ttyACM0', 115200, timeout = 10)
+#listen for input like on Google
 while True:
-	read_serial=ser.readline()
-	s[0] = str(int (ser.readline(),16))
-	print(s[0])
-	print(read_serial)
+    line = ser.readline()
+    if len(line) == 0:
+        print("Nothing to see here")
+    else:
+        print(line)
+        
+#turn this into a while loop
+# ser = serial.Serial('/dev/ttyACM0',9600)
+# s = [0,1]
+# while True:
+# 	read_serial=ser.readline()
+# 	s[0] = str(int (ser.readline(),16))
+# 	print(s[0])
+# 	print(read_serial)
 
